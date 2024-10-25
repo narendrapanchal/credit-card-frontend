fc9cc0e94edaaaf2c1b84e78fd6e3632ed19276b
inno <narendrapanchal020@gmail.com>
2024-10-25 10:38:23 +0530

    cockei

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,25 +1,32 @@
 import { useState, createContext } from "react";
 import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 
 export const UserContext = createContext();
+
 const UserContextProvider = ({ children }) => {
   const navigate = useNavigate();
   const [login, setLogin] = useState(
-    { token: localStorage.getItem("token") } || null
+    { token: Cookies.get("token") } || null
   );
+
   const handleLogin = (data) => {
-    localStorage.setItem("token", "Bearer " + data);
-    setLogin({ token: "Bearer " + data });
+    const token = "Bearer " + data;
+    Cookies.set("token", token, { expires: 1 }); 
+    setLogin({ token });
   };
+
   const handleLogout = () => {
     setLogin(null);
-    localStorage.removeItem("token");
+    Cookies.remove("token");
     navigate("/");
   };
+
   return (
     <UserContext.Provider value={{ handleLogin, login, handleLogout }}>
       {children}
     </UserContext.Provider>
   );
 };
+
 export default UserContextProvider;
